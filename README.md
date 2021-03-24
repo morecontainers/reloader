ee03599562258462f6dbd5ba14b6c4b3fcd163f0
Henrik Holst <hholst80@gmail.com>
2021-03-24 12:13:58 +0100

    Initial commit
    Should work with Prometheus/VictoraMetrics & HAProxy.
    Not in any way tested and validated for a production deployment.

---
+++ README.md
@@ -0,0 +1,102 @@
+# Pod Reloader
+
+TODO:
+
+* Proper logging
+
+## Example deployment
+
+Kubernetes still does not have a formal sidecar pattern,
+so the deployment is a bit verbose.
+
+```yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: prometheus
+spec:
+  selector:
+    matchLabels:
+      app: prometheus
+  template:
+    metadata:
+      labels:
+        app: prometheus
+    spec:
+      initContainers:
+      - name: reloader-init
+        image: docker.io/morecontainers/reloader:latest
+        env:
+        - {name: RELOADER_INIT,      value: "1"}
+        - {name: RELOADER_CONFIGMAP, value: prometheus}
+        - {name: RELOADER_PATH,      value: /config}
+        - {name: RELOADER_ENDPOINT,  value: "http://localhost:8428/-/reload"}
+      containers:
+      - name: prometheus
+        image: victoriametrics/victoria-metrics:v1.56.0
+        args: ["-promscrape.config","/config/prometheus.yml"]
+        ports:
+        - containerPort: 8428
+        volumeMounts:
+        - name: config-volume
+          mountPath: /config
+      - name: reloader
+        image: docker.io/morecontainers/reloader:latest
+        env:
+        - {name: RELOADER_CONFIGMAP, value: prometheus}
+        - {name: RELOADER_PATH,      value: /config}
+        - {name: RELOADER_ENDPOINT,  value: "http://localhost:8428/-/reload"}
+        volumeMounts:
+        - name: config-volume
+          mountPath: /config
+      volumes:
+      - name: config-volume
+        emptyDir: {}
+```
+
+```
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: prometheus
+data:
+  prometheus.yml: |
+    global:
+      scrape_interval: 5s
+    scrape_configs:
+     - job_name: haproxy
+       static_configs:
+       - targets: ['haproxy:8428']
+```
+
+NOTE:  You need to setup the service account to have view permissions on the configmap,
+as described in RBAC section below.
+
+## Prebuilt containers
+
+Prebuilt docker containers available at Docker Hub:
+
+https://hub.docker.com/repository/docker/morecontainers/reloader
+
+## RBAC
+
+https://kubernetes.io/docs/reference/access-authn-authz/rbac/#service-account-permissions
+
+```sh
+kubectl create rolebinding serviceaccounts-view \
+  --clusterrole=view \
+  --group=system:serviceaccounts:default \
+  --namespace=default
+```
+
+## Application support
+
+TODO: Describe the integration paths in some more detail.
+
+* [Prometheus](https://github.com/prometheus/prometheus/issues/1572)
+* [NGINX](https://www.nginx.com/resources/wiki/start/topics/tutorials/commandline/)
+* [HAProxy](https://www.haproxy.com/blog/haproxy-process-management/)
+
+## Authors
+
+* [Henrik Holst](mailto:hholst80@gmail.com)
---
+++ reloader.py
@@ -0,0 +1,109 @@
+import json
+import logging
+import os
+import signal
+import binascii
+
+import requests
+
+INIT = os.environ.get("RELOADER_INIT", False)
+NAMESPACE = os.environ.get("RELOADER_NAMESPACE", "default")
+CONFIGMAP = os.environ.get("RELOADER_CONFIGMAP")
+SECRET = os.environ.get("RELOADER_SECRET")
+PATH = os.environ["RELOADER_PATH"]
+ENDPOINT = os.environ.get("RELOADER_ENDPOINT")
+PIDFILE = os.environ.get("RELOADER_PIDFILE")
+SIGNAL = signal.Signals[os.environ.get("RELOADER_SIGNAL", "SIGUSR2")].value
+
+assert bool(CONFIGMAP) ^ bool(SECRET)
+
+
+def watch():
+    e_type = "configmaps" if CONFIGMAP else "secrets"
+    e_name = CONFIGMAP or SECRET
+    if os.path.isfile("/run/secrets/kubernetes.io/serviceaccount/token"):
+        k8s_host = os.environ["KUBERNETES_SERVICE_HOST"]
+        k8s_port = os.environ["KUBERNETES_SERVICE_PORT_HTTPS"]
+        url = "https://%s:%s/api/v1/watch/namespaces/%s/%s/%s" % (
+            k8s_host,
+            k8s_port,
+            NAMESPACE,
+            e_type,
+            e_name,
+        )
+        with open("/var/run/secrets/kubernetes.io/serviceaccount/token", "rb") as f:
+            token = f.read()
+        headers = {"Authorization": "Bearer %s" % token.decode()}
+        cacert = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+        r = requests.get(url, stream=True, headers=headers, verify=cacert)
+    else:
+        url = "http://localhost:8001/api/v1/watch/namespaces/%s/%s/%s" % (
+            NAMESPACE,
+            e_type,
+            e_name,
+        )
+        r = requests.get(url, stream=True)
+    r.raise_for_status()
+    print("Watching %s" % url)
+    for line in r.iter_lines():
+        e_type, e_object = json.loads(line).values()
+        yield (e_type, e_object)
+
+
+def update_base64(config):
+    for key, data in config["data"].items():
+        path = os.path.join(PATH, key)
+        print("Updating config %s" % path)
+        data = binascii.a2b_base64(data)
+        with open(path, "wb") as f:
+            f.write(data)
+
+
+def update(config):
+    for key, data in config["data"].items():
+        path = os.path.join(PATH, key)
+        print("Updating config %s" % path)
+        with open(path, "wb") as f:
+            f.write(data.encode())
+
+
+def reload():
+    if ENDPOINT:
+        try:
+            requests.post(ENDPOINT)
+        except:
+            logging.exception("Reload endpoint failure:")
+    elif PIDFILE:
+        try:
+            with open(PIDFILE, "rb") as f:
+                pid = int(f.read())
+            os.kill(pid, SIGNAL)
+        except:
+            logging.exception("Reload signalling failure:")
+    else:
+        pass
+
+
+if __name__ == "__main__":
+    print("Reloader settings:")
+    print("NAMESPACE: %s" % NAMESPACE)
+    if CONFIGMAP:
+        print("CONFIGMAP: %s" % CONFIGMAP)
+    if SECRET:
+        print("SECRET:    %s" % SECRET)
+    if ENDPOINT:
+        print("ENDPOINT:  %s" % ENDPOINT)
+    elif PIDFILE:
+        print("PIDFILE:   %s" % ENDPOINT)
+        print("SIGNAL:    %i" % SIGNAL)
+    stream = watch()
+    if not INIT:
+        next(stream)
+    for e_type, e_object in stream:
+        if SECRET:
+            update_base64(e_object)
+        else:
+            update(e_object)
+        if INIT:
+            break
+        reload()
